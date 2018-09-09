f94e3609cdace7ff4fd97e14efff76f991d1dde4
ternary-se <github@default.nu>
2018-09-09 12:02:15 +0200

    mowerfynctions.py, mowersensors.py and mower should be the only required files to run the robot now
    mowerfynctions.py, mowersensors.py and mower should be the only required files to run the robot now
    
    Switched to python3 and curses which allowed an event loop that also could read non-blocking from stdin.
    Stille some things to do but this version looks really promising in doing things The Right Way

---
+++ bin/mowersensors.py
@@ -0,0 +1,167 @@
+import sys,os
+import curses
+import time
+import datetime
+
+import RPi.GPIO as GPIO
+import atexit
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+from numpy import median
+from collections import deque
+import mowerfunctions
+from random import *
+import syslog
+
+##mower stuff
+GPIO.setmode(GPIO.BCM)
+def cleanGPIO():
+    GPIO.cleanup()
+atexit.register(cleanGPIO)
+# BWF pins and variables
+GPIO.setup(24, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(25, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+SPI_PORT   = 0
+SPI_DEVICE = 0
+mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+#wheelload_readings = deque([0, 0, 0, 0, 0])
+wheelload_readings = deque([0, 0, 0,])
+cutterload_readings = deque([0, 0, 0, 0, 0])
+
+runmode = "manual"
+bypasscounter = 0
+rplacement = "inside"
+lplacement = "inside"
+
+### used variables ###
+voltage_readings = deque([0, 0, 0, 0, 0])
+wheelload_readings = deque([0, 0, 0,])
+cutterload_readings = deque([0, 0, 0, 0, 0])
+
+class Sensorread(object):
+
+    global voltage_readings
+    global wheelload_readings
+    global cutterload_readings
+    
+    def readvoltage(self):
+        a0 = mcp.read_adc(0)
+        # put a0 first in the list and make a median value for stability
+        voltage_readings.rotate(1)
+        voltage_readings[0] = a0
+        a0_median = median(voltage_readings)
+        voltage = (float(a0_median) * 3.2680645161290322580645161290323 * 4.0228013029315960912052117263844) / 1023
+        return voltage
+
+    def readcoils(self):
+        # read # of hits of right BWF coil during 0.1s
+        rhitcount = 0
+        lhitcount = 0
+        prev_rinput = 0
+        prev_linput = 0
+        bwf_start = time.time()
+        elapsed_bwf = time.time() - bwf_start
+        while (elapsed_bwf <= .10):
+          #take a reading
+          rinput = GPIO.input(24)
+          linput = GPIO.input(25)
+          if ((not prev_rinput) and rinput):
+            rhitcount += 1
+          if ((not prev_linput) and linput):
+            lhitcount += 1
+          prev_rinput = rinput
+          prev_linput = linput
+          elapsed_bwf = time.time() - bwf_start
+
+        hitcount = [lhitcount, rhitcount]
+        return hitcount
+
+    def readmotorcurrent(self):
+        wheelload_readings.rotate(1)
+        wheelload_readings[0] = mcp.read_adc(2)
+        a2_median = median(wheelload_readings)
+        return a2_median
+
+    def readcuttercurrent(self):
+        cutterload_readings.rotate(1)
+        cutterload_readings[0] = mcp.read_adc(4)
+        a4_median = median(cutterload_readings)
+        return a4_median
+
+### escapes: make shure the robot really stops before changing direction to avoid great currents
+
+    def escapehitrightcoil(self):
+        syslog.syslog("Liam-on-RPi: entering escapehitrightcoil")
+        robot = mowerfunctions.Robot()
+        robot.stop()
+        time.sleep(1.85)
+        # read a low value for initialising variable
+        wheelload = self.readmotorcurrent()
+        wheelload = self.readmotorcurrent()
+        wheelload = self.readmotorcurrent()
+        wheelload = self.readmotorcurrent()
+        #assumes the coast is clear backwards
+        robot.backward(255, 1.5)
+        robot.stop()
+        time.sleep(0.85)
+        turntime = random() * 1.35
+        #syslog.syslog("Liam-on-RPi: escapehitrightcoil turntime = " + str(turntime))
+        robot.left(255)
+        fromtime = time.time()
+        a = 0
+        # loop acts like sleep but does sensor reading while turning
+        while (a == 0):
+          timediff = time.time() - fromtime
+          # wait a little while before reading motor current to stabilise it
+          if (timediff > 0.2):
+              wheelload = self.readmotorcurrent()
+          coil = self.readcoils()
+          # if left coil passes BWF, times up or we hit something
+          if ( 17 <= coil[0] <= 27) or (timediff >= turntime) or (wheelload > 400):
+            robot.stop()
+            time.sleep(0.85)
+            a = 1
+            if (timediff >= turntime):
+              returncode = "ok"
+            else:
+              returncode = "fail"
+            syslog.syslog("Liam-on-RPi: escapehitrightcoil obstacle, leftcoil = " + str(coil[0]) + ", timediff = " + str(timediff) + "/" + str(turntime) + ", wheel = " + str(wheelload) + " returned " + returncode)
+            return returncode
+
+
+    def escapehitleftcoil(self):
+        syslog.syslog("Liam-on-RPi: entering escapehitleftcoil")
+        robot = mowerfunctions.Robot()
+        robot.stop()
+        time.sleep(1.85)
+        # read a low value for initialising variable
+        wheelload = self.readmotorcurrent()
+        wheelload = self.readmotorcurrent()
+        wheelload = self.readmotorcurrent()
+        wheelload = self.readmotorcurrent()
+        #assumes the coast is clear backwards
+        robot.backward(255, 1.5)
+        robot.stop()
+        time.sleep(0.85)
+        turntime = random() * 1.35
+        robot.right(255)
+        fromtime = time.time()
+        a = 0
+        # loop acts like sleep but does sensor reading while turning
+        while (a == 0):
+          timediff = time.time() - fromtime
+          # wait a little while before reading motor current to stabilise it
+          if (timediff > 0.2):
+              wheelload = self.readmotorcurrent()
+          coil = self.readcoils()
+          # if right coil passes BWF, times up or we hit something
+          if ( 17 <= coil[1] <= 27) or (timediff >= turntime) or (wheelload > 400):
+            robot.stop()
+            time.sleep(0.85)
+            a = 1
+            if (timediff >= turntime):
+              returncode = "ok"
+            else:
+              returncode = "fail"
+            syslog.syslog("Liam-on-RPi: escapehitleftcoil obstacle, rightcoil = " + str(coil[1]) + ", timediff = " + str(timediff) + "/" + str(turntime) + ", wheel = " + str(wheelload) + " returned " + returncode)
+            return returncode
