16778441ceff67571be6a93f16deec8e7cb8eb5e
ternary-se <github@default.nu>
2018-09-26 20:59:51 +0200

    needed files for testing/running this hack
    assumes directory structure like $HOME/LIAM-on-RPi/bin ...

--- bin/mowersensors.py
+++ bin/mowersensors.py
@@ -19,6 +19,7 @@ def cleanGPIO():
     GPIO.cleanup()
 atexit.register(cleanGPIO)
 # BWF pins and variables
+GPIO.setup(22, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(24, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(25, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 SPI_PORT   = 0
@@ -38,11 +39,14 @@ voltage_readings = deque([0, 0, 0, 0, 0])
 wheelload_readings = deque([0, 0, 0,])
 cutterload_readings = deque([0, 0, 0, 0, 0])
 
+robot = mowerfunctions.Robot(stop_at_exit = False)
+
 class Sensorread(object):
 
     global voltage_readings
     global wheelload_readings
     global cutterload_readings
+    global robot
     
     def readvoltage(self):
         a0 = mcp.read_adc(0)
@@ -54,26 +58,32 @@ class Sensorread(object):
         return voltage
 
     def readcoils(self):
-        # read # of hits of right BWF coil during 0.1s
+        # read # of hits of BWF coils during 0.1s
         rhitcount = 0
         lhitcount = 0
+        bhitcount = 0
         prev_rinput = 0
         prev_linput = 0
+        prev_binput = 0
         bwf_start = time.time()
         elapsed_bwf = time.time() - bwf_start
         while (elapsed_bwf <= .10):
           #take a reading
-          rinput = GPIO.input(24)
-          linput = GPIO.input(25)
+          rinput = GPIO.input(25)
+          linput = GPIO.input(24)
+          binput = GPIO.input(22)
           if ((not prev_rinput) and rinput):
             rhitcount += 1
           if ((not prev_linput) and linput):
             lhitcount += 1
+          if ((not prev_binput) and binput):
+            bhitcount += 1
           prev_rinput = rinput
           prev_linput = linput
+          prev_binput = binput
           elapsed_bwf = time.time() - bwf_start
 
-        hitcount = [lhitcount, rhitcount]
+        hitcount = [lhitcount, rhitcount, bhitcount]
         return hitcount
 
     def readmotorcurrent(self):
@@ -92,7 +102,7 @@ class Sensorread(object):
 
     def escapehitrightcoil(self):
         syslog.syslog("Liam-on-RPi: entering escapehitrightcoil")
-        robot = mowerfunctions.Robot()
+        #mb robot = mowerfunctions.Robot(stop_at_exit = False)
         robot.stop()
         time.sleep(1.85)
         # read a low value for initialising variable
@@ -101,8 +111,31 @@ class Sensorread(object):
         wheelload = self.readmotorcurrent()
         wheelload = self.readmotorcurrent()
         #assumes the coast is clear backwards
-        robot.backward(255, 1.5)
-        robot.stop()
+
+        #that was dumb robot.backward(255, 1.5)
+        #that was dumb robot.stop()
+        robot.backward(255)
+        fromtime = time.time()
+        a = 0
+        # loop acts like sleep but does sensor reading while turning
+        turntime = 1.5
+        while (a == 0):
+          timediff = time.time() - fromtime
+          # wait a little while before reading motor current to stabilise it
+          if (timediff > 0.2):
+              wheelload = self.readmotorcurrent()
+          coil = self.readcoils()
+          # if rear coil passes BWF, times up or we hit something
+          if ( 17 <= coil[2] <= 27) or (timediff >= turntime) or (wheelload > 600):
+            robot.stop()
+            #time.sleep(0.85)
+            a = 1
+            if (timediff >= turntime):
+              returncode = "reverse ok"
+            else:
+              returncode = "reverse fail"
+            syslog.syslog("Liam-on-RPi: escapehitrightcoil reverse, rearcoil = " + str(coil[2]) + ", timediff = " + str(timediff) + "/" + str(turntime) + ", wheel = " + str(wheelload) + " returned " + returncode)
+
         time.sleep(0.85)
         turntime = random() * 1.35
         #syslog.syslog("Liam-on-RPi: escapehitrightcoil turntime = " + str(turntime))
@@ -117,7 +150,7 @@ class Sensorread(object):
               wheelload = self.readmotorcurrent()
           coil = self.readcoils()
           # if left coil passes BWF, times up or we hit something
-          if ( 17 <= coil[0] <= 27) or (timediff >= turntime) or (wheelload > 400):
+          if ( 17 <= coil[0] <= 27) or (timediff >= turntime) or (wheelload > 600):
             robot.stop()
             time.sleep(0.85)
             a = 1
@@ -131,7 +164,7 @@ class Sensorread(object):
 
     def escapehitleftcoil(self):
         syslog.syslog("Liam-on-RPi: entering escapehitleftcoil")
-        robot = mowerfunctions.Robot()
+        #mb robot = mowerfunctions.Robot(stop_at_exit = False)
         robot.stop()
         time.sleep(1.85)
         # read a low value for initialising variable
@@ -140,8 +173,30 @@ class Sensorread(object):
         wheelload = self.readmotorcurrent()
         wheelload = self.readmotorcurrent()
         #assumes the coast is clear backwards
-        robot.backward(255, 1.5)
-        robot.stop()
+        #that was dumb robot.backward(255, 1.5)
+        #robot.stop()
+        robot.backward(255)
+        fromtime = time.time()
+        a = 0
+        # loop acts like sleep but does sensor reading while turning
+        turntime = 1.5
+        while (a == 0):
+          timediff = time.time() - fromtime
+          # wait a little while before reading motor current to stabilise it
+          if (timediff > 0.2):
+              wheelload = self.readmotorcurrent()
+          coil = self.readcoils()
+          # if rear coil passes BWF, times up or we hit something
+          if ( 17 <= coil[2] <= 27) or (timediff >= turntime) or (wheelload > 600):
+            robot.stop()
+            #time.sleep(0.85)
+            a = 1
+            if (timediff >= turntime):
+              returncode = "reverse ok"
+            else:
+              returncode = "reverse fail"
+            syslog.syslog("Liam-on-RPi: escapehitrightcoil reverse, rearcoil = " + str(coil[2]) + ", timediff = " + str(timediff) + "/" + str(turntime) + ", wheel = " + str(wheelload) + " returned " + returncode)
+
         time.sleep(0.85)
         turntime = random() * 1.35
         robot.right(255)
@@ -155,7 +210,7 @@ class Sensorread(object):
               wheelload = self.readmotorcurrent()
           coil = self.readcoils()
           # if right coil passes BWF, times up or we hit something
-          if ( 17 <= coil[1] <= 27) or (timediff >= turntime) or (wheelload > 400):
+          if ( 17 <= coil[1] <= 27) or (timediff >= turntime) or (wheelload > 600):
             robot.stop()
             time.sleep(0.85)
             a = 1
